043bedd5b1ac68ea32ffb2329cfd4c0effe7066d
Dan Kolb <dan.kolb@rackspace.com>
2015-04-21 01:25:31 -0500

    added comments to make sense of quick hack

--- volume.sh
+++ volume.sh
@@ -1,17 +1,19 @@
 #! /bin/bash
 #quick script (little flag checking) for volume control
+#Dan Kolb 2015 <dan@dankolb.net>
+#usage:
+#volume.sh <up|down> <5|1|mute>
+
+#get min/max limits as well as the current level used
 limits_min=$(amixer get Master | grep "Limits:" | awk '{print $3}')
 limits_max=$(amixer get Master | grep "Limits:" | awk '{print $5}')
 cur_vol=$(amixer get Master | grep "Front Left:" | awk '{print $4}')
 
-#echo "cur $cur_vol"
-#echo "min $limits_min"
-#echo "max $limits_max"
-
+#find out what 5% of the limit will be
 percent=$(echo "$limits_max * 0.05" | bc)
+#divide by 1 to get rid of decimal
 percent=$(echo "$percent / 1" | bc)
 
-#echo $percent
 
 if [[ $2 -eq "5" ]] 
 	then
@@ -22,9 +24,12 @@ if [[ $2 -eq "1" ]]
 	percent="0.01"
 fi
 
+#same as above
+#did I mention this was done quickly with little flag checking?
 per_change=$(echo "$limits_max * $percent" | bc)
 per_change=$(echo "$per_change / 1" | bc)
 
+#grab first argument to know whether togo up or down
 if [[ $1 == "up" ]]
 	then
 	new_lev=$(echo "$cur_vol + $per_change" | bc)
@@ -34,8 +39,11 @@ if [[ $1 == "down" ]]
 	new_lev=$(echo "$cur_vol - $per_change" | bc)
 fi
 
+#and check if it is to be muted. (afterthough)
 if [[ $2 == "mute" ]]
 	then
 	new_lev=0
 fi	
+
+#run the actual command
 amixer cset iface=MIXER,name="Master Playback Volume" $new_lev 1>/dev/null
