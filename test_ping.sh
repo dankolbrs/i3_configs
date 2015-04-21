1eef515430fa5d61a3d4db3539f36d2a82bd11e3
Dan Kolb <dan.kolb@rackspace.com>
2015-04-21 08:05:00 -0500

    added conditional to throw error for DNS failure

--- test_ping.sh
+++ test_ping.sh
@@ -2,5 +2,11 @@
 #quick ping test
 #Dan Kolb 2015 <dan@dankolb.net>
 url="google.com"
-ping -c1 $url | grep -Po "time=\d*\.\d*" | awk -F= '{print $2}'
+response=$(ping -c1 $url 2>&1 | grep -Po "time=\d*\.\d*" | awk -F= '{print $2}')
+if [[ $response == "" ]] 
+	then
+	echo "999"
+else
+	echo $response
+fi
 
